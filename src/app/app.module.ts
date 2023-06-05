484975e4082cda15381a6b5e14346628fa56c663
yash donda <yashdonda007@gmail.com>
2023-06-05 12:24:26 +0530

    ui for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccountModule } from './account/account.module'
-
+import { environment } from "../environments/environment";
+import { AngularFireModule } from '@angular/fire';
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFireModule } from "@angular/fire";
+// import { AngularFirestoreModule } from "@angular/fire/firestore";/
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +17,11 @@ import { AccountModule } from './account/account.module'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AccountModule
+    AccountModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig),
+    // AngularFireModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig),
+    // AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
